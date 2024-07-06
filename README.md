a2dbcacf10c1c2e7463e704e2f01e64365dbf61e
kadix <kadix@p12.localdomain>
2024-07-05 18:04:38 -0600

    trimgrade.sh: better quoting, user specified output filename/format

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bin
 handy doodads from my ~/bin\
-i aim to make these plug-and-play but I cannot guarantee these will work on your system. For educational purposes only.
+i aim to make these plug-and-play but i cannot guarantee these will work on your system. For educational purposes only.
 
 ## trimgrade.sh
 trim x seconds from the end of a video, apply haldclut color grading, and output 'out.mkv'
--- trimgrade.sh
+++ trimgrade.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-## trim x seconds from the end of a video, apply haldclut color grading, and output 'out.mkv'
+## trim x seconds from the end of a video, apply haldclut color grading, and output user specified filename/format
 ##
 ## example:
-## trimgrade.sh DSC_0425.MOV 4 ./lut/fuji_provia_100.png
+## trimgrade.sh DSC_0425.MOV 4 ./lut/fuji_provia_100.png out.mkv
 
 inputDur=`ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "$1"`
-outputDur=`bc <<< $inputDur-$2`
-ffmpeg -i $1 -map 0 -t $outputDur -vf "movie=$3, [in] haldclut" out.mkv
+outputDur=`bc <<< "$inputDur"-"$2"`
+ffmpeg -i "$1" -map 0 -t "$outputDur" -vf "movie="$3", [in] haldclut" "$4"
