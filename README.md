07590ef8c124571b6867298188a230439f7d40b6
kadix <kadix@p12.localdomain>
2024-07-05 18:43:11 -0600

    trimgrade.sh: cleaned up formatting, better quotation around vars, copy exif from source file, auto detect ffmpeg hwaccel

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # bin
 handy doodads from my ~/bin\
-i aim to make these plug-and-play but i cannot guarantee these will work on your system. For educational purposes only.
+i aim to make these plug-and-play but i cannot guarantee these will work on your system. For educational purposes only
 
 ## trimgrade.sh
-trim x seconds from the end of a video, apply haldclut color grading, and output 'out.mkv'
+trim x seconds from the end of a video, apply haldclut color grading, and output user specified filename/format copying exif data from source file
 
 example:\
 `trimgrade.sh DSC_0425.MOV 4 ./lut/fuji_provia_100.png`
--- trimgrade.sh
+++ trimgrade.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-## trim x seconds from the end of a video, apply haldclut color grading, and output user specified filename/format
+## trim x seconds from the end of a video, apply haldclut color grading, and output user specified filename/format copying exif data from source file
 ##
 ## example:
 ## trimgrade.sh DSC_0425.MOV 4 ./lut/fuji_provia_100.png out.mkv
 
 inputDur=`ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "$1"`
 outputDur=`bc <<< "$inputDur"-"$2"`
-ffmpeg -i "$1" -map 0 -t "$outputDur" -vf "movie="$3", [in] haldclut" "$4"
+
+ffmpeg -hwaccel auto -y -i "$1" -map 0 -t "$outputDur" -vf "movie=$3, [in] haldclut" "$4"
+
+exiftool -verbose -TagsFromFile "$4" "$1"
+
+rm -vf "$1"_original
